0743ee5da88f7738ce15d1264d295b041ddc0ddd
Johanna Orsholm <johanna.orsholm@slu.se>
2024-09-26 15:00:14 +0200

    Updated load function to new data

--- coi/load_FinBOL_GBOL.R
+++ coi/load_FinBOL_GBOL.R
@@ -1,32 +1,20 @@
 
 #' Loads FinBOL data (train) and GBOL data (test) as listed dataframes. 
 #' @param level lowest taxonomic level (genus/species)
-load_FinBOL_GBOL <- function(level = "species", short = F){
+load_FinBOL_GBOL <- function(train_file = "data/train_tax.tsv",
+                             test_file = "data/test_tax.tsv", 
+                             ranks = c("Class", 
+                                       "Order",
+                                       "Family",
+                                       "Subfamily",
+                                       "Tribe",
+                                       "Genus",
+                                       "Species")){
   
-  shorttxt <- ""
-  if(short) shorttxt <- "short"
+  data_train <- read.delim(train_file)
+  data_test <- read.delim(test_file)
   
-  data_train <- seqinr::read.fasta(paste0("data/finbol-gbol/train_finbol-gbol_", 
-                                          level, ".fasta"), 
-                                   whole.header = T)
-  data_test <- seqinr::read.fasta(paste0("data/finbol-gbol/test_finbol-gbol_", 
-                                         level, ".fasta"), 
-                                  whole.header = T)
+  colnames(data_train) <- colnames(data_test) <- c(colnames(data_train)[1], ranks)
   
-  ref_seq <- data.frame(do.call("rbind", stringr::str_split(names(data_train), " |[|]")),
-                        unlist(unname(lapply(data_train,
-                                             function(x) stringr::str_to_upper(
-                                               stringr::str_c(x, sep = "", collapse = ""))))))
-  ref_seq_test <- data.frame(do.call("rbind", stringr::str_split(names(data_test), " |[|]")),
-                             unlist(unname(lapply(data_test,
-                                                  function(x) stringr::str_to_upper(
-                                                    stringr::str_c(x, sep = "", collapse = ""))))))
-  
-  if(level == "species"){
-    colnames(ref_seq) = colnames(ref_seq_test) = c("ID", "Class", "Order", "Family", "Subfamily", "Genus", "Species", "DNA")
-  } else if(level == "genus"){
-    colnames(ref_seq) = colnames(ref_seq_test) = c("ID", "Class", "Order", "Family", "Subfamily", "Genus", "DNA")
-  }
-  
-  return(list("train" = ref_seq, "test"= ref_seq_test))
+  return(list("train" = data_train, "test"= data_test))
 }
