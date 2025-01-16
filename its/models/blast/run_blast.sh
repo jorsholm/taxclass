5323f641e587745a199bec73025de43cc7bd0de6
brendanf <brendan.furneaux@gmail.com>
2025-01-16 15:04:06 +0200

    modify blast script for ITS

--- its/models/blast/run_blast.sh
+++ its/models/blast/run_blast.sh
@@ -57,38 +57,15 @@ do
                -qcov_hsp_perc 80
 done
 
-# train the amino acid model
-TRAIN_FILE=$DATA/train_aa_sintax.fasta
-MODEL_FILE=train_aa_$SLURM_ARRAY_TASK_ID
-
-$TIME makeblastdb -in $TRAIN_FILE\
-                  -dbtype prot\
-                  -out $MODEL_FILE
-
-# classify the amino acid test sequences
-for TEST in test testshort
-do
-TEST_FILE=$DATA/${TEST}_aa.fasta
-RAW_FILE=${MODEL}_${TEST}_aa_$SLURM_ARRAY_TASK_ID.raw
-
-$TIME blastp -query $TEST_FILE\
-             -db $MODEL_FILE\
-             -out $RAW_FILE\
-             -num_threads $SLURM_CPUS_PER_TASK\
-             -outfmt "6 delim=; qseqid sseqid"\
-             -max_target_seqs 1\
-             -qcov_hsp_perc 80
-done
-
 # reformat the output - top hit
 for f in *$SLURM_ARRAY_TASK_ID.raw
 do
   RESULT_FILE=${f%.raw}.tsv
   RESULT_FILE=$RESULTS/blast_top_hit${RESULT_FILE#blast}
 
-  echo "ID	class	Prob_class	order	Prob_order	"\
-       "family	Prob_family	subfamily	Prob_subfamily	"\
-       "tribe	Prob_tribe	genus	Prob_genus	species	Prob_species"\
+  echo "ID	kingdom	Prob_kingdom	phylum	Prob_phylum	"\
+       "class	Prob_class	order	Prob_order	"\
+       "family	Prob_family	genus	Prob_genus	species	Prob_species"\
        >$RESULT_FILE
 
   sed -r 's/([^;]+);[^;]+;tax=/\1/; s/[kpcofgs]:([^,]+),?/\t\1\t1.0/g'\
@@ -102,12 +79,12 @@ do
   RESULT_FILE=${f%.raw}.tsv
   RESULT_FILE=$RESULTS/blast_thresh${RESULT_FILE#blast}
 
-  echo "ID	class	Prob_class	order	Prob_order	"\
-       "family	Prob_family	subfamily	Prob_subfamily	"\
-       "tribe	Prob_tribe	genus	Prob_genus	species	Prob_species"\
+  echo "ID	kingdom	Prob_kingdom	phylum	Prob_phylum	"\
+       "class	Prob_class	order	Prob_order	"\
+       "family	Prob_family	genus	Prob_genus	species	Prob_species"\
        >$RESULT_FILE
   awk -F"[,;]"    'BEGIN {
-       n=split("90,93,95,96,96.5,97,98", thresh)
+       n=split("65,70,75,80,85,90,98", thresh)
      }
      NR>1 {printf "%s", $1
        sub(/tax=/, "")
