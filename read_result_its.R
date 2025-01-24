4b72fc63bc3312d52cb3bf442b466acb011c25af
Johanna Orsholm <johanna.orsholm@slu.se>
2025-01-24 17:40:14 +0100

    Added RDP result to ITS

--- read_result_its.R
+++ read_result_its.R
@@ -1,12 +1,10 @@
 # Read and modify model results, because it is becoming too much for the same script 
 
-setwd("its")
 rm(list = ls())
 
 # LOAD FUNCTIONS ---------------------------------------------------------------
 
 library(tidyverse)
-source("load_FinBOL_GBOL.R")
 source("functions.R")
 
 # SET PARAMETERS ---------------------------------------------------------------
@@ -25,13 +23,15 @@ if(keep_na) natxt <- "_keepNA"
 # LOAD DATA --------------------------------------------------------------------
 
 # Load test and train data 
-data <- load_FinBOL_GBOL(ranks = c("Kingdom", 
-                                   "Phylum", 
-                                   "Class", 
-                                   "Order", 
-                                   "Family", 
-                                   "Genus", 
-                                   "Species"))
+data <- load_train_test(train_file = "its/data/train_tax.tsv", 
+                        test_file = "its/data/test_tax.tsv", 
+                        ranks = c("Kingdom",
+                                  "Phylum",
+                                  "Class",
+                                  "Order",
+                                  "Family",
+                                  "Genus",
+                                  "Species"))
 
 # Replace labels of taxa unique to test
 # Sort test data in alphabetical order
@@ -49,11 +49,11 @@ data_true <- get_data_true(data$test, data$train,
 #   dplyr::select(Kingdom, Phylum) |> 
 #   dplyr::distinct()
 
-train_tax <- 
-  read.table("data/train_tax.tsv", 
-             header = T, 
-             fill = T) |> 
-  dplyr::distinct(kingdom, phylum, class, order, family, genus, species) 
+# train_tax <- 
+#   read.table("its/data/train_tax.tsv", 
+#              header = T, 
+#              fill = T) |> 
+#   dplyr::distinct(kingdom, phylum, class, order, family, genus, species) 
 
 
 # CORRECT COLUMN NAMES AND ORDER -----------------------------------------------
@@ -67,7 +67,7 @@ correct_cols <- c("ID",
 
 # BayesANT
 result_BayesANT <-
-  read.table(paste0("results/bayesant/bayesant_test", shorttxt, "_nt_16.tsv"),
+  read.table(paste0("its/results/bayesant/bayesant_test", shorttxt, "_nt_16.tsv"),
              header = T) |>
   tibble::rownames_to_column("ID") |>
   dplyr::arrange(ID)
@@ -90,12 +90,11 @@ result_BayesANT <-
 result_BayesANT <- arrange_columns(result_BayesANT, correct_cols)
 
 # RDP
-# result_RDP <-
-#   read.table(paste0("results/rdp/rdp_test", shorttxt, "_nt_aln_label.txt"),
-#              header = T) |>
-#   tibble::rownames_to_column("ID") |>
-#   dplyr::arrange(ID)
-# result_RDP <- arrange_columns(result_RDP, correct_cols)
+result_RDP <-
+  read.table(paste0("its/results/rdp_nbc/rdp_nbc_test", shorttxt, "_1.tsv"),
+             header = T) |>
+  dplyr::arrange(ID)
+result_RDP <- arrange_columns(result_RDP, correct_cols)
 
 # PROTAX
 # result_PROTAX <-
@@ -109,38 +108,16 @@ result_BayesANT <- arrange_columns(result_BayesANT, correct_cols)
 # # Rename Protax columns
 # colnames(result_PROTAX) <- colnames(result_BayesANT)
 
-# EPA-ng Taxonomy tree
-# result_epatax <-
-#   read.table(paste0("results/epang_taxtree/epang_taxtree_test", shorttxt,
-#                     "_nt_all.tsv"),
-#              header = T) |>
-#   dplyr::arrange(ID)
-# result_epatax <- rename_unk_output(result_epatax)
-# result_epatax <- arrange_columns(result_epatax, correct_cols)
-
 # Sintax
 result_SINTAX <-
-  read.table(paste0("results/sintax/sintax_test", shorttxt, "_nt_16.tsv"),
+  read.table(paste0("its/results/sintax/sintax_test", shorttxt, "_nt_16.tsv"),
              header = TRUE) |> 
   dplyr::arrange(ID)
 result_SINTAX <- arrange_columns(result_SINTAX, correct_cols)
 
-# EPA-ng phylogenetic tree 
-# result_epaphyl <- 
-#   read.table(paste0("results/epang_phyltree/epang_phyltree_test",
-#                     shorttxt, "_finbol-gbol.txt"),
-#              header = T, sep = "\t")  |>
-#   dplyr::arrange(ID)
-# result_epaphyl$class[which(result_epaphyl$class == "DISTANT")] <- "unk"
-# result_epaphyl$species <- sapply(result_epaphyl$species, 
-#                                  function(x) stringr::str_replace(x, " ", "_"),
-#                                  USE.NAMES = F)
-# result_epaphyl <- rename_unk_output(result_epaphyl)
-# result_epaphyl <- arrange_columns(result_epaphyl, correct_cols)
-
 # MycoAI-CNN 
 result_aicnn <-
-  read.table(paste0("results/mycoai_cnn/mycoai_cnn_test", shorttxt,
+  read.table(paste0("its/results/mycoai_cnn/mycoai_cnn_test", shorttxt,
                     "_nt_40.tsv"),
              header = T) |>
   dplyr::arrange(ID)
@@ -152,7 +129,7 @@ result_aicnn <- arrange_columns(result_aicnn, correct_cols)
 
 # MycoAI-BERT 
 result_aibert <-
-  read.table(paste0("results/mycoai_bert/mycoai_bert_test", shorttxt,
+  read.table(paste0("its/results/mycoai_bert/mycoai_bert_test", shorttxt,
                     "_nt_gpu.tsv"),
              header = T) |>
   dplyr::arrange(ID)
@@ -164,7 +141,7 @@ result_aibert <- arrange_columns(result_aibert, correct_cols)
 
 # BLAST top hit 
 result_blast_top <-
-  read.table(paste0("results/blast/blast_top_hit_test", shorttxt, "_nt_16.tsv"),
+  read.table(paste0("its/results/blast/blast_top_hit_test", shorttxt, "_nt_16.tsv"),
              header = T) |> 
   dplyr::arrange(ID) |> 
   dplyr::mutate(species = purrr::map_chr(species, 
@@ -175,7 +152,7 @@ result_blast_top <- arrange_columns(result_blast_top, correct_cols)
 
 # BLAST threshold
 result_blast_thresh <-
-  read.table(paste0("results/blast/blast_thresh_test", shorttxt, "_nt_16.tsv"),
+  read.table(paste0("its/results/blast/blast_thresh_test", shorttxt, "_nt_16.tsv"),
              header = T) |> 
   dplyr::arrange(ID)
 result_blast_thresh <- arrange_columns(result_blast_thresh, correct_cols)
@@ -187,7 +164,7 @@ if(!keep_na){
 
 # DNA-barcoder
 result_dnabarcoder <-
-  read.table(paste0("results/dnabarcoder/test", shorttxt, "_nt_40.tsv"),
+  read.table(paste0("its/results/dnabarcoder/test", shorttxt, "_nt_40.tsv"),
              header = T) |>
   dplyr::arrange(ID)
 if(keep_na){
@@ -211,7 +188,7 @@ result_dnabarcoder <- arrange_columns(result_dnabarcoder, correct_cols)
 
 # IDTAXA 
 result_idtaxa <-
- read.table(paste0("results/idtaxa/idtaxa_test", shorttxt, "_nt_4.tsv"), 
+ read.table(paste0("its/results/idtaxa/idtaxa_test", shorttxt, "_nt_4.tsv"), 
             fill = T, header = T) |> 
   dplyr::arrange(ID)
 result_idtaxa <- arrange_columns(result_idtaxa, correct_cols)
@@ -255,7 +232,7 @@ if(keep_na){
 
 # Crest4
 result_crest4 <- 
-  read.table(paste0("results/crest4/crest4_test", shorttxt, "_nt_4.tsv"), 
+  read.table(paste0("its/results/crest4/crest4_test", shorttxt, "_nt_4.tsv"), 
              header = T) |> 
   dplyr::arrange(ID)
 if(keep_na){
@@ -265,66 +242,6 @@ if(keep_na){
 }
 result_crest4[paste0("Prob_", ranks)] <- 1
 result_crest4 <- arrange_columns(result_crest4, correct_cols)
-
-
-# Mystery result ------
-# # Brendan's mystery model
-# # Special keep_na case: 
-# # Unknown = novel (has probability), NA = NA (no probability)
-# # if the rank above is unknown, then the whole row should be unknown with the same probability.
-# # When not keep_na, the same rule applies for all unknown and NAs. 
-# result_mystery <- read.table("models/mystery/mystery_model_finbol_gbol.txt",
-#                              header = T) |> 
-#   dplyr::arrange(ID)
-# result_mystery <- arrange_columns(result_mystery, correct_cols)
-# if(keep_na){
-#   result_mystery <- 
-#     result_mystery |> 
-#     dplyr::mutate(across(all_of(ranks), 
-#                          ~stringr::str_replace(., "^unknown_.*", "unk"))) |> 
-#     tidyr::pivot_longer(cols = all_of(ranks),
-#                         names_to = "rank",
-#                         values_to = "taxon") |>
-#     tidyr::pivot_longer(cols = paste0("Prob_", ranks),
-#                         names_to = "Prob_rank",
-#                         names_pattern = "Prob_(.*)",
-#                         values_to = "Prob") |> 
-#     dplyr::filter(rank == Prob_rank) |> 
-#     dplyr::group_by(ID) |> 
-#     dplyr::mutate(taxon = dplyr::if_else(is.na(taxon) & any(taxon == "unk"), "unk", taxon)) |> 
-#     dplyr::mutate(Prob_filled = dplyr::if_else(taxon == "unk" & is.na(Prob), dplyr::lag(Prob, default = NA), Prob)) |> 
-#     tidyr::fill(Prob_filled, .direction = "down") |> 
-#     dplyr::mutate(Prob = dplyr::if_else(taxon == "unk" & is.na(Prob), Prob_filled, Prob)) |> 
-#     dplyr::select(-Prob_filled) |> 
-#     dplyr::ungroup() |> 
-#     dplyr::select(-Prob_rank) |> 
-#     tidyr::pivot_wider(names_from = rank,
-#                        values_from = c(taxon, Prob)) |> 
-#     dplyr::rename_with(~gsub("taxon_", "", .), dplyr::starts_with("taxon_")) 
-# }else{
-#   result_mystery <- 
-#     result_mystery |> 
-#     dplyr::mutate(across(all_of(ranks), 
-#                          ~stringr::str_replace(., "^unknown_.*", "unk"))) |> 
-#     tidyr::pivot_longer(cols = all_of(ranks),
-#                         names_to = "rank",
-#                         values_to = "taxon") |>
-#     tidyr::pivot_longer(cols = paste0("Prob_", ranks),
-#                         names_to = "Prob_rank",
-#                         names_pattern = "Prob_(.*)",
-#                         values_to = "Prob") |> 
-#     dplyr::filter(rank == Prob_rank) |> 
-#     dplyr::group_by(ID) |> 
-#     dplyr::mutate(taxon = dplyr::if_else(is.na(taxon), "unk", taxon)) |>  
-#     dplyr::mutate(Prob = dplyr::if_else(is.na(Prob), dplyr::lag(Prob, default = NA), Prob)) |> 
-#     tidyr::fill(Prob, .direction = "down") |> 
-#     dplyr::ungroup() |> 
-#     dplyr::select(-Prob_rank) |> 
-#     tidyr::pivot_wider(names_from = rank,
-#                        values_from = c(taxon, Prob)) |> 
-#     dplyr::rename_with(~gsub("taxon_", "", .), dplyr::starts_with("taxon_")) 
-# }
-# result_mystery <- rename_unk_output(result_mystery)
 
 # UGLY FIX FOR BLAST MISSING DATA ----------------------------------------------
 
@@ -347,13 +264,10 @@ result_blast_top <- rbind(result_blast_top, add_blast_top) |> dplyr::arrange(ID)
 results <- list(
   "BayesANT" = result_BayesANT,
   #  "PROTAX" = result_PROTAX,
-  #"EPA-ng taxtree" = result_epatax,
-  #"RDP" = result_RDP,
+  "RDP" = result_RDP,
   #"BLAST top hit" = result_blast_top, 
   #"BLAST threshold" = result_blast_thresh, 
   "SINTAX" = result_SINTAX,
-  # "Mystery" = result_mystery,
-  #"EPA-ng phyltree" = result_epaphyl,
   "DNABarcoder" = result_dnabarcoder,
   "IDTAXA" = result_idtaxa, 
   "MycoAI-CNN" = result_aicnn, 
@@ -363,29 +277,4 @@ results <- list(
 
 results <- lapply(results, function(x) as.data.frame(x))
 
-saveRDS(results, paste0("result_list", natxt, ".rds"))
-
-# CHECK TAXONOMY ---------------------------------------------------------------
-
-# mismatch_taxonomy <- check_taxonomy(taxdf = train_tax, results = results, ranks = ranks)
-# 
-# library(tidyverse)
-# 
-# mismatch_taxonomy |> 
-#   as_tibble() |> 
-#   filter(str_starts(model, "MycoAI") | model == "SINTAX") |> 
-#   mutate(intid = row_number()) |> 
-#   left_join(train_tax, by = "Species", suffix = c("_pred", "_train")) |> 
-#   relocate(intid, .before = Class_pred) |> 
-#   relocate(model, .after = intid) |> 
-#   relocate(Species, .after = model) |>  
-#   pivot_longer(4:15, 
-#                names_to = c("rank", "set"), 
-#                values_to = "taxa", 
-#                names_sep = "_") |> 
-#   pivot_wider(names_from = set, values_from = taxa) |> 
-#   filter(pred != train)
-# 
-#  mismatch_taxonomy |> 
-#    as_tibble() |> 
-#    filter(str_starts(model, "MycoAI") | model == "SINTAX")
+saveRDS(results, paste0("its/result_list", natxt, ".rds"))
