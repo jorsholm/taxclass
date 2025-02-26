308331069713797e79e778d1a06e193fef212501
brendanf <brendan.furneaux@gmail.com>
2025-02-26 15:13:35 +0200

    fixes to model size script

--- scripts/model_sizes.sh
+++ scripts/model_sizes.sh
@@ -3,6 +3,9 @@
 # find sizes of all models
 # this is quite idiosyncratic because the models do not store everything in the same file structure
 
+# the model files are not checked in to github, so this needs to be run on a machine which
+# has actually run all of the training steps
+
 OUTFILE="results/model_sizes.tsv"
 rm -f OUTFILE
 mkdir -p $(dirname $OUTFILE)
@@ -12,7 +15,7 @@ touch $OUTFILE
 ls -s1 */models/bayesant/*.rds | sed 's/^ *//' >>$OUTFILE
 
 # BLAST: index is in multiple files, not divided into directories
-ls -s1 */models/blast/*.n?? | sed 's/^ *//' >>$OUTFILE
+ls -s1 */models/blast/*.[np]?? | sed 's/^ *//' >>$OUTFILE
 
 # CREST4: model is in directory
 du -s */models/crest4/train_??_* | tr "\t" " " >>$OUTFILE
@@ -21,16 +24,16 @@ du -s */models/crest4/train_??_* | tr "\t" " " >>$OUTFILE
 ls -s1 */models/dnabarcoder/train_??_*/train_??_*.cutoffs.best.json | sed 's/^ *//' >>$OUTFILE
 
 # IDTAXA: model is a single rds file
-ls -s1 */models/idtaxa/*.rds | sed 's/^ *//' >>$OUTFILE
+ls -s1 */models/idtaxa/train_??_.*.rds | sed 's/^ *//' >>$OUTFILE
 
 # MycoAI: model is a single pt file
 ls -s1 */models/mycoai_*/train_??_*.pt | sed 's/^ *//' >>$OUTFILE
 
 # Protax-A: model is multiple files in a directory
-du -s */models/protax-a/protax-a_*_tax_1 | tr "\t" " " >>$OUTFILE
+du -s */models/protax-a/train_nt_*_tax_1 | tr "\t" " " >>$OUTFILE
 
 # RDP-NBC: model is multiple files in a directory
-du -s */models/rdp_nbc/rdp_nbc_1 | tr "\t" " " >>$OUTFILE
+du -s */models/rdp_nbc/train_nt_1 | tr "\t" " " >>$OUTFILE
 
 # SINTAX: index is a single udb file
 ls -s1 */models/sintax/train_??_*.udb | sed 's/^ *//' >>$OUTFILE
